f6efb495590b888e33c4d30326d349a9c6d36745
benjamin <benschac@gmail.com>
2026-05-18 14:49:27 -0400

    mermaid type

---
+++ apps/presentation/types/reveal-js-mermaid-plugin.d.ts
@@ -0,0 +1,6 @@
+declare module "reveal.js-mermaid-plugin/plugin/mermaid/mermaid.esm.js" {
+  import type { RevealPlugin } from "reveal.js";
+
+  const RevealMermaid: RevealPlugin;
+  export default RevealMermaid;
+}
